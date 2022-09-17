efc2a16f00d87dc218008a069533b480257964a2
the00sheep <schepp.andreas@gmail.com>
2022-09-17 14:35:27 +0300

    Added ORDER BY and Soft Delete functionality

--- App/Controllers/hotelController.php
+++ App/Controllers/hotelController.php
@@ -7,10 +7,11 @@ use \App\Models\Hotels;
 
 
 /**
- * Signup controller
+ * Hotel controller
  *
  * PHP version 7.0
  */
+
 class hotelController extends \Core\Controller
 {
   /**
@@ -24,9 +25,22 @@ class hotelController extends \Core\Controller
       $hotelList = Hotels::getAllHotels();
       //var_dump($hotelList);
       //exit();
-      View::renderTemplate('Hotels/new.html', [
+      View::renderTemplate('hotels/new.html', [
         'hotels' => $hotelList
     ]);
+  }
+
+  public function listAction($orderBy = 0){ 
+        
+    if(isset($_GET['orderBy']) && !empty($_GET['orderBy'])){
+      $orderBy = $_GET['orderBy'];
+    }
+  
+    $hotelList = Hotels::getAllHotels($orderBy);
+
+    View::renderTemplate('hotels/list.html', [
+      'hotels' => $hotelList,
+   ]);
   }
 
   public function createAction()
@@ -35,9 +49,68 @@ class hotelController extends \Core\Controller
    
     
     var_dump($_POST);
-    //$hotels->save();
+   
+    if($hotels->save()){
+      
+      //post /redirect / get pattern to avoid resubmission
+      //303 See Other redirect status response code indicates that the redirects don't link to the requested resource itself, but to another page
+      header('Location: http://' . $_SERVER['HTTP_HOST']. '/hotelController/success', true, 303);
+      exit;
 
-    //View::renderTemplate('Signup/success.html');
+    }
+    //else if validation fails render the new view, passing the error messages
+    else{
+      View::renderTemplate('Hotels/new.html', [
+        'hotels' => $hotels
+      ]);
+    }    
 
+  }
+
+  /**
+   * Soft delete, sets status to 0
+   */
+
+  /**
+   * TBD error messages
+   * merge the two functions
+   * return to view with saved order preference
+   * ajax, redirect
+   */
+  public function deleteHotelAction(){
+    if(isset($_GET['id']) && !empty($_GET['id'])){
+      $id = $_GET['id'];
+    }
+
+    $deleteVal = Hotels::deleteHotel($id);
+
+    $hotelList = Hotels::getAllHotels();
+    if($deleteVal){
+      View::renderTemplate('Hotels/list.html',[
+        'hotels' => $hotelList,
+     ]);
+    }
+    
+  }
+
+  public function activateHotel(){
+    if(isset($_GET['id']) && !empty($_GET['id'])){
+      $id = $_GET['id'];
+    }
+
+    $activateVal = Hotels::deleteHotel($id, 1);
+
+    $hotelList = Hotels::getAllHotels();
+    if($activateVal){
+      View::renderTemplate('Hotels/list.html',[
+        'hotels' => $hotelList,
+     ]);
+    }
+    
+  }
+
+  public function successAction()
+  {
+    View::renderTemplate('Hotels/success.html');
   }
 }
--- App/Models/Hotels.php
+++ App/Models/Hotels.php
@@ -8,6 +8,16 @@ use \App\Token;
 use \Core\View;
 use \App\Mail;
 use \App\Paginator;
+use Attribute;
+
+/**
+ * Error messages
+ * 
+ * @var array
+ *
+ */
+
+$errors = [];
 
 
 class Hotels extends \Core\Model
@@ -33,12 +43,51 @@ class Hotels extends \Core\Model
      *
      * @return array  An array of all the hotel records
      */
-    public static function getAllHotels()
+    public static function getAllHotels($orderBy = 0)
     {
-        $db = static::getDB();
+        
+        //$columns =['name', 'address', 'city', 'country', 'zipcode', 'website', 'email', 'vergeprice'];
 
-        $sql = 'SELECT * FROM hotels
-                ORDER BY name';
+        $db = static::getDB();
+        
+       
+        switch ($orderBy) {
+            case "name":
+                $sql = 'SELECT * FROM hotels
+                        ORDER BY name';
+                break;
+            case "address":
+                $sql = 'SELECT * FROM hotels
+                        ORDER BY address';
+                break;
+            case "city":
+                $sql = 'SELECT * FROM hotels
+                        ORDER BY city';
+                break;
+            case 'country':
+                $sql = 'SELECT * FROM hotels
+                        ORDER BY country';
+                break;
+            case 'zipcode':
+                $sql = 'SELECT * FROM hotels
+                        ORDER BY zipcode';  
+                break;
+             case 'website':
+                $sql = 'SELECT * FROM hotels
+                        ORDER BY website';
+                break;
+            case 'email':
+                $sql = 'SELECT * FROM hotels
+                        ORDER BY email';
+                  break;
+            case 'vergeprice':
+                $sql = 'SELECT * FROM hotels
+                        ORDER BY vergeprice';     
+                break;
+            default:  $sql = 'SELECT * FROM hotels';
+        }
+
+              
 
         $stmt = $db->prepare($sql);
 
@@ -49,24 +98,103 @@ class Hotels extends \Core\Model
         return $result;
     }
 
+ 
     public function save()
     {
-     
-        $sql = 'INSERT INTO hotels (name, address, city, zipcode, website, email, verge_price, status)
-                VALUES (:name, :address, :city, :zipcode, :website, :email, :verge_price, :status)';
-    
-        $db = static::getDB();
-        $stmt = $db->prepare($sql);
-    
-        $stmt->bindValue(':name',       $this->name, PDO::PARAM_STR);
-        $stmt->bindValue(':address',    $this->address, PDO::PARAM_STR);
-        $stmt->bindValue(':city',       $this->city, PDO::PARAM_STR);
-        $stmt->bindValue(':zipcode',    $this->zipcode, PDO::PARAM_STR);
-        $stmt->bindValue(':website',    $this->website, PDO::PARAM_STR);
-        $stmt->bindValue(':email',      $this->email, PDO::PARAM_STR);
-        $stmt->bindValue(':verge_price',$this->verge_price, PDO::PARAM_STR);
-        $stmt->bindValue(':status',     $this->status, PDO::PARAM_STR);
-    
-        $stmt->execute();
+        $this->validate();
+
+        if (empty($this->errors)){
+   
+            //sql insert new hotel into hotels table
+            $sql = 'INSERT INTO hotels (name, address, city, country, zipcode, website, email, vergeprice, status)
+                VALUES (:name, :address, :city, :zipcode, :country, :website, :email, :vergeprice, 1)';
+        
+            $db = static::getDB();
+            $stmt = $db->prepare($sql);
+        
+            $stmt->bindValue(':name',       $this->name, PDO::PARAM_STR);
+            $stmt->bindValue(':address',    $this->address, PDO::PARAM_STR);
+            $stmt->bindValue(':city',       $this->city, PDO::PARAM_STR);
+            $stmt->bindValue(':country',    $this->country, PDO::PARAM_STR);
+            $stmt->bindValue(':zipcode',    $this->zipcode, PDO::PARAM_STR);
+            $stmt->bindValue(':website',    $this->website, PDO::PARAM_STR);
+            $stmt->bindValue(':email',      $this->email, PDO::PARAM_STR);
+            $stmt->bindValue(':vergeprice', $this->vergeprice, PDO::PARAM_STR);
+            $stmt->bindValue(':status',     $this->status, PDO::PARAM_STR);
+                
+
+            //PDO method returns true on success, false on failure 
+            return $stmt->execute();
+        }
+        //validation failed
+        return false;
     }
+
+    public static function deleteHotel($id, $active = 0){
+             
+        if ($active == 0){
+        $sql = "UPDATE hotels 
+                SET    status = 0
+                WHERE  hotel_id ='{$id}'";
+        }
+        else {
+            $sql = "UPDATE hotels 
+            SET    status = 1
+            WHERE  hotel_id ='{$id}'";
+        }
+
+        $db = static::getDB();   
+        $stmt = $db->prepare($sql);
+
+        return $stmt->execute();
+    }
+
+ 
+    /**
+     * validates current property
+     * error gets added to $errors 
+     * 
+     * name, adress, city, country req
+     * email valid
+     * verge price > 0
+     *  
+     * @return void
+     */
+
+
+    public function validate()
+    {
+        //name required
+        if ($this->name == ''){
+            $this->error[] = 'Name is required'; 
+        }
+
+        if ($this->adress == ''){
+            $this->error[] = 'Adress is required'; 
+        }
+
+        if ($this->city == ''){
+            $this->error[] = 'City is required'; 
+        }
+
+        if ($this->country == ''){
+            $this->error[] = 'Country is required'; 
+        }
+
+        //email validation using fiter_var method
+        if (filter_var($this->email, FILTER_VALIDATE_EMAIL) === false){
+            $this->errors[] = 'Invalid email';
+        }
+
+        //verge price > 0
+        if($this->vergeprice < 0){
+            $this->errors[] = 'Verge Price must be greater than 0';
+        }
+
+ 
+    }
+
+    
+    
+  
 }
